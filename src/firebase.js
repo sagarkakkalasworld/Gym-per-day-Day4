4900f5d9a50d85e2df7fe1be7f8b6d671452511d
sagarkakkalasworld <sagarkakkalasworld@gmail.com>
2026-02-21 20:05:43 +0000

    Fix formatting of firebaseConfig apiKey

--- src/firebase.js
+++ src/firebase.js
@@ -4,7 +4,7 @@ import { getFirestore } from "firebase/firestore";
 
 //get firebase credentials from Firebase App/Video
 const firebaseConfig = {
-  apiKey: "${{ secrets.API_KEY }}"
+  apiKey: "${{ secrets.API_KEY }}",
   authDomain: "${{ secrets.AUTH_DOMAIN }}",
   projectId: "${{ secrets.PROJECT_ID }}",
   storageBucket: "${{ secrets.STORAGE_BUCKET }}",
